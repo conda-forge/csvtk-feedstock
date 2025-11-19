7ae0067b27fd4939cd5dc49487883d946dd5cd0b
Wei Shen <shenwei356@gmail.com>
2025-11-19 19:12:27 +0800

    create a copy of the binary file -- tsvtk

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,11 @@ cd csvtk
 go build -buildmode=pie -trimpath -o="%LIBRARY_BIN%\csvtk.exe" -ldflags="-s -w -X main.Version=%PKG_VERSION%" || goto :error
 go-licenses save . --save_path=license-files  --ignore github.com/ajstarks/svgo --ignore github.com/golang/freetype/raster || goto :error
 
+
+REM Create a copy named tsvtk.
+REM When invoked as "tsvtk", csvtk will automatically enable the "-t/--tabs" flag.
+copy /Y "%LIBRARY_BIN%\csvtk.exe" "%LIBRARY_BIN%\tsvtk.exe"
+
 goto :EOF
 
 :error
--- recipe/build.sh
+++ recipe/build.sh
@@ -5,3 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 cd csvtk
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/csvtk -ldflags="-s -w -X main.Version=${PKG_VERSION}"
 go-licenses save . --save_path=license-files --ignore github.com/ajstarks/svgo --ignore github.com/golang/freetype/raster
+
+# Create a copy named tsvtk.
+# When invoked as "tsvtk", csvtk will automatically enable the "-t/--tabs" flag.
+cp ${PREFIX}/bin/csvtk ${PREFIX}/bin/tsvtk
