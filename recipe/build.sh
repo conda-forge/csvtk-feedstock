295a4ef9af3bdee749892898ee72109ba98393b3
Antônio Camargo <apcamargo@users.noreply.github.com>
2026-06-05 10:32:56 -0300

    Install shell completions

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,12 @@ cd csvtk
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/csvtk -ldflags="-s -w -X main.Version=${PKG_VERSION}"
 go-licenses save . --save_path=license-files --ignore github.com/ajstarks/svgo --ignore github.com/golang/freetype/raster
 
+# Install shell completions
+mkdir -p ${PREFIX}/share/zsh/site-functions ${PREFIX}/share/bash-completion/completions ${PREFIX}/share/fish/vendor_completions.d
+${PREFIX}/bin/csvtk completion --shell zsh --file ${PREFIX}/share/zsh/site-functions/_csvtk
+${PREFIX}/bin/csvtk completion --shell bash --file ${PREFIX}/share/bash-completion/completions/csvtk
+${PREFIX}/bin/csvtk completion --shell fish --file ${PREFIX}/share/fish/vendor_completions.d/csvtk.fish
+
 # Create a symbolic link named tsvtk.
 # When invoked as "tsvtk", csvtk will automatically enable the "-t/--tabs" flag.
 cd ${PREFIX}/bin/
